cf4b19a5ff5b7a353e2494a97fbdfb0774fa1663
phil <phil.taenzler@gmail.com>
2019-11-30 00:48:05 +0100

    more tokenization

--- text_miner/stemming_vs_lemmatization/tokenlemma.py
+++ text_miner/stemming_vs_lemmatization/tokenlemma.py
@@ -1,10 +1,39 @@
 #taken from
 #https://www.analyticsvidhya.com/blog/2018/02/the-different-methods-deal-text-data-predictive-python/
+#https://www.machinelearningplus.com/nlp/lemmatization-examples-python/
 
 import pandas as pd
 from nltk.corpus import stopwords
 stop = stopwords.words('english')
 from textblob import Word
+import nltk
+from nltk.stem import WordNetLemmatizer 
+
+nl_lem = WordNetLemmatizer()
+
+from nltk.corpus import wordnet
+
+#pip install is broken. first, pip install pdfminer==20140328. then install pattern. yes, this version. 
+
+import pattern
+from pattern.en import lemma, lexeme
+
+
+#for textblob
+tag_dict = {"J": 'a',
+                "N": 'n',
+                "V": 'v',
+                "R": 'r'}
+
+def get_wordnet_pos(word):
+    """Map POS tag to first character lemmatize() accepts"""
+    tag = nltk.pos_tag([word])[0][1][0].upper()
+    tag_dict = {"J": wordnet.ADJ,
+                "N": wordnet.NOUN,
+                "V": wordnet.VERB,
+                "R": wordnet.ADV}
+
+    return tag_dict.get(tag, wordnet.NOUN)
 
 
 from textblob import TextBlob
@@ -13,13 +42,27 @@ print(wrong)
 print(str(TextBlob(wrong).correct()))
 #correct does not work inplace...
 lem = "the quick brown fox jumped over the dawg. well better good goodly bad badly jump jumps jumping jumped person people train trains"
+#lem = "The striped bats are hanging on their feet for best"
 print(lem)
-lem = TextBlob(lem)
-print(lem.tags)
+lem2 = TextBlob(lem)
+print(lem2.tags)
+words_and_tags = [(x, tag_dict.get(pos[0], 'n')) for x, pos in lem2.tags]    
+lemmatized_list = [wd.lemmatize(tag) for wd, tag in words_and_tags]
 lemmat = []
 for x in lem.split():
     lemmat.append(Word(x).lemmatize())
+print("wordblob lemmatize:")
 print(lemmat)
+print("nltk lemmatize via tags")
+nltk_lem_tags = [nl_lem.lemmatize(x, get_wordnet_pos(x)) for x in nltk.word_tokenize(lem)]
+print(nltk_lem_tags)
+print(" ".join(nltk_lem_tags))
+print("wordblob lemmatize + tags")
+print(lemmatized_list)
+print(" ".join(lemmatized_list))
+print("pattern lemmatize + tags")
+print([lemma(x) for x in lem.split()])
+print(" ".join([lemma(x) for x in lem.split()]))
 exit(1)
 
 
