f4932fb6ed54175206ea22f23b21be1ea0575703
phil <phil.taenzler@gmail.com>
2020-12-15 10:07:20 +0100

    why take responsibility when you have BASH?

---
+++ scripts/coin_flipper.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+for i in {0..2}
+do
+  SUM=0
+  for j in {0..3}
+  do
+    TEMP=$(echo "$RANDOM % 2" | bc)
+    SUM1=$(echo "$SUM + $TEMP" | bc)
+    SUM=$SUM1
+  done
+  if [ $SUM -gt 1 ]; then
+    echo "Series $i - True!"
+  else
+    echo "Series $i - FALSE!"
+  fi
+done
+
